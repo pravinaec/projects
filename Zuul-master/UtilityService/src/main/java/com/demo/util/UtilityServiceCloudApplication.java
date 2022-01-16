d9bc4e0c1c79f5d5db3f221bfb8314522db27985
pravinss.aec@gmail.com <pravinss.aec@gmail.com>
2022-01-16 13:30:16 +0530

    Springboot with EurekaServer and zuul for load balance and routing

---
+++ Zuul-master/UtilityService/src/main/java/com/demo/util/UtilityServiceCloudApplication.java
@@ -0,0 +1,28 @@
+package com.demo.util;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.loadbalancer.LoadBalanced;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.context.annotation.Bean;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
+import org.springframework.web.client.RestTemplate;
+
+@SpringBootApplication
+@EnableJpaRepositories
+@EnableEurekaClient
+//@EntityScan("com.us.models.*")   
+public class UtilityServiceCloudApplication {
+
+    public static void main( String[] args ) {
+        SpringApplication.run( UtilityServiceCloudApplication.class, args );
+    }
+
+
+    @Bean
+    @LoadBalanced
+    RestTemplate restTemplate( ) {
+
+        return new RestTemplate( );
+    }
+}
